bc0e12a2ec3b9a85f9e6768049c3fc8f0c2df004
Kunj Mungalpara <148608404+kunj24@users.noreply.github.com>
2025-10-24 22:49:39 +0530

    Remove certificate section completely - deleted all certificate components, types, and assets

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import AboutSection from '@/components/sections/AboutSection'
 import ProjectsSection from '@/components/sections/ProjectsSection'
 import SkillsSection from '@/components/sections/SkillsSection'
 import EducationSection from '@/components/sections/EducationSection'
-import CertificatesSection from '@/components/sections/CertificatesSection'
 import ContactSection from '@/components/sections/ContactSection'
 
 export default function Home() {
@@ -27,10 +26,6 @@ export default function Home() {
 
       <section id="projects">
         <ProjectsSection />
-      </section>
-
-      <section id="certificates">
-        <CertificatesSection />
       </section>
 
       <section id="contact">
